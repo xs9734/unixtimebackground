76b344ec203be4859843441534c73cb7f5e61700
David <42820350+xs9734@users.noreply.github.com>
2019-01-18 01:53:47 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var intervlID = window.setInterval(secondTick, 1000);
+
+function secondTick() {
+    var ts = Math.round((new Date()).getTime() / 1000);
+    console.log(ts);
+    //document.getElementById("number").innerHTML = ts;
+    var num = ts;
+    var n = num.toString();
+    var id = n;
+    var hexValue = id.substr(id.length - 6);
+    document.getElementById("hexNum").innerHTML = "Current Time is <br> <br>" + "#" + hexValue;
+    document.body.style.backgroundColor = "#" + hexValue;
+}
+
+
