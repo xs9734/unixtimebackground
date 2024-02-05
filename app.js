037806101f7d60dbcd6f45ca0a377cc988971670
David <42820350+xs9734@users.noreply.github.com>
2024-02-04 17:51:41 -0800

    Convert UNIX time to HEX
    -Simplified the script
    -UNIX time now is converted to Hex before being pasted so now it is truly a Hex Color clock :)

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var intervlID = window.setInterval(secondTick, 1000);
+setInterval(secondTick, 1000);
 
 function secondTick() {
-    var ts = Math.round((new Date()).getTime() / 1000);
-    console.log(ts);
-    //document.getElementById("number").innerHTML = ts;
-    var num = ts;
-    var n = num.toString();
-    var id = n;
-    var hexValue = id.substr(id.length - 6);
-    document.getElementById("hexNum").innerHTML = "Current Time is <br> <br>" + "#" + hexValue;
-    document.body.style.backgroundColor = "#" + hexValue;
+    const ts = Math.round(new Date().getTime() / 1000);
+    const hexValue = ts.toString(16).slice(-6);
+    
+    document.getElementById("hexNum").innerHTML = `Current Time is <br> <br>#${hexValue}`;
+    document.body.style.backgroundColor = `#${hexValue}`;
     document.body.style.transition = "all 1s";
 }
-
-
